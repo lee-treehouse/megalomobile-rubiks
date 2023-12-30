9a4a2ca497bb610c9f720a976742c0e2ce2533b3
leentaylor <leentaylor@gmail.com>
2023-12-30 15:31:19 +1100

    start work on rays and readcube script

---
+++ Assets/ReadCube.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadCube : MonoBehaviour
+{
+
+    public Transform tUp;
+    public Transform tDown;
+    public Transform tFront;
+    public Transform tBack;
+    public Transform tLeft;
+    public Transform tRight;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
