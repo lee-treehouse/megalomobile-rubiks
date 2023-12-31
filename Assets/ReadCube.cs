4e27de06652c993873dab77e1d10466e5f233e4c
leentaylor <leentaylor@gmail.com>
2023-12-31 15:55:20 +1100

    start of first ray

--- Assets/ReadCube.cs
+++ Assets/ReadCube.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.PhysicsModule;
 
 public class ReadCube : MonoBehaviour
 {
@@ -14,16 +15,32 @@ public class ReadCube : MonoBehaviour
 
     // mysterious thing to go with putting prefab cube faces on a layer
     private int layerMask = 1 << 8; 
-    
+
+    CubeState cubeState;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cubeState = FindObjectOfType<CubeState>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        List<GameObject> facesHit = new List<GameObject>();
+        Vector3 ray = tFront.transform.position;
+        print(ray);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, tFront.right, out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(ray, tFront.right * hit.distance, Color.yellow);
+            facesHit.Add(hit.collider.gameObject);
+        } else {
+            Debug.DrawRay(ray, tFront.right * 1000, Color.green);
+        }
+
     }
 }
